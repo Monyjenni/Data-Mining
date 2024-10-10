77f01813537e0517b121e00a3148bac0f35f4605
Monyjenni <chansovanmonyyoeun03@gmail.com>
2024-10-10 11:22:36 +0700

    feat: set_up

---
+++ Assignment_01/as1.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
+import seaborn as sns
+# We can override the default matplotlib styles with those of Seaborn
+sns.set()
